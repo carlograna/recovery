1bf8f967f47655ed1f086af9e064f86b7245cfa4
Carlo <carlograna@hotmail.com>
2018-03-29 16:52:57 -0500

    Created Batch Factory, clean up the code

---
+++ ReadCsv/BatchFactory.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReadCsv
+{
+    public class BatchFactory
+    {
+
+
+        public static Batch GetBatch(string batchType, IEnumerable<BankTran> bankTrans)
+        {
+            if (batchType == "recovery")
+            {
+                return CreateRecoveryBatch(bankTrans);
+            }
+            else
+                throw new Exception("Batch Type is not recognized");
+        }
+
+        private static Batch CreateRecoveryBatch(IEnumerable<BankTran> bankTrans)
+        {
+
+            DateTime curDate = DateTime.Now;
+            int globalBatchID;
+            double amount;
+            double totalAmt = 0;
+            int lockboxID;
+            int bankID;
+            int checkCount;
+            int stubCount;
+
+            lockboxID = bankTrans.Any(x => x.RecoveryPaymentType == "OVP") ? 316 : 306; // overpaid batch or BRI batch
+            bankID = 7;
+            checkCount = bankTrans.Count();
+            stubCount = bankTrans.Count();
+
+            using (var dbContext = new ItemDBContext())
+            {
+                // Key is equal to the current max value plus one
+                globalBatchID = dbContext.Batch.Max(b => b.GlobalBatchID) + 1;
+            }
+
+            foreach (var tran in bankTrans)
+            {
+                if (double.TryParse(tran.PaymentAmount, out amount))
+                {
+                    totalAmt += amount;
+                }
+                else throw new Exception("Unable to parse transaction amount: " + tran.PaymentAmount);
+            }
+            Batch batch = new Batch()
+            {
+                GlobalBatchID = globalBatchID,
+                BankID = bankID,
+                LockboxID = lockboxID,
+                DESetupID = null,
+                ProcessingDate = curDate.Date,
+                BatchID = 203,
+                DepositStatus = 850,
+                BatchCueID = 0,// ???,
+                CutOffStatus = 0,
+                BatchMode = null,
+                SystemType = 0,
+                Priority = 1,
+                PrintLabels = 1,
+                BatchTypeCode = 50, // ???,
+                BatchTypeSubCode = 0,
+                BatchTypeText = null,
+                ConsolidationStatus = 0,
+                CARStatus = null,
+                KFIStatus = null,
+                FAXStatus = null,
+                FloatAssignmentStatus = 0,
+                ScanStatus = 21,
+                DEStatus = 0,
+                ExtractSequenceNumber = 0,
+                ImageExtractSequenceNumber = null,
+                DepositDate = curDate.Date,
+                CheckCount = checkCount,
+                CheckAmount = (decimal)totalAmt,
+                StubCount = stubCount,
+                StubAmount = (decimal)totalAmt,
+                CreationDate = curDate,
+                BillingTC = 0,
+                BillingRT = 0,
+                BillingAccount = 0,
+                BillingSerial = 0,
+                WorkgroupID = 1,
+                CurrencyCode = null,
+                MICRVerifyStatus = 4,
+                DEPrinted = null,
+                Is2Pass = 1,
+                UseCar = 1,
+                NoChecksEnclosed = 0,
+                Rejects = 0,
+                DepositTicketPrinted = false,
+                DateLastUpdated = curDate,
+                TransportID = null,
+                TrayID = null,
+                SubTrayID = null,
+                UseCustomer = 0,
+                DepositDDA = null,
+                EncodeComplete = null,
+                SplitsEnded = false,
+                SiteCode = null,
+                BitonalImageRemovalDate = null, // ist seems this removal dates are a 1 1/2 month later,
+                DataRemovalDate = null,
+                ModificationDate = curDate,
+                TransportBatchID = null,
+                GrayScaleImageRemovalDate = null,
+                ColorImageRemovalDate = null,
+                CWDBDataRemovalDate = null,
+                CWDBBitonalImageRemovalDate = null,
+                CWDBGrayScaleImageRemovalDate = null,
+                CWDBColorImageRemovalDate = null,
+                BatchName = null,
+                BatchTypeID = Guid.NewGuid(),
+                DecisionStatus = 0,
+                AllowDecisioning = false,
+                RequireDEBeforeTwoPass = false,
+                OriginalGlobalBatchID = null,
+                BatchExtractID = null,
+                IsImageExchange = false,
+                CaptureSiteCodeID = 10,
+                BatchSourceKey = 1,
+                NumOLDDaysRolled = 0,
+            };
+
+            return batch;
+        }
+    }
+}
--- ReadCsv/Default.aspx.cs
+++ ReadCsv/Default.aspx.cs
@@ -89,8 +89,7 @@ namespace ReadCsv
                                    PaymentAmount = c.Element("PaymentAmount").Value,
                                    DueDate = c.Element("DueDate").Value,
                                    Parameter = c.Descendants("Parameter"),
-                                   FirstName = c.Descendants("FirstName"),
-                                   LastName = c.Descendants("LastName")
+                                   Contact = c.Descendants("ContactInformation"),
                                };
 
             IEnumerable<BankTran> bankTrans = BankTrans(transactions);
@@ -146,20 +145,22 @@ namespace ReadCsv
 
         private void CreateTransOnDB(List<IEnumerable<BankTran>> List)
         {
-            try
-            {
+            //try
+            //{
                 using (ItemDBContext dbContext = new ItemDBContext())
                 {
                     foreach (var subList in List)
                     {
-                        Batch batch = CreateBatch(subList);
+                        //Batch batch = CreateBatch(subList);
+                        Batch batch = BatchFactory.GetBatch("recovery", subList);                       
+                    
                         BatchDataEntry bde = CreateBatchDataEntry(batch.GlobalBatchID);
 
                         IEnumerable<Checks> checks = CreateChecks(subList, batch);
                         IEnumerable<ChecksDataEntry> checksDE = CreateChecksDataEntry(checks, batch, subList.ToList());
                         IEnumerable<Transactions> trans = CreateTransactions(checks, batch);
 
-                        dbContext.Batch.Add(batch);
+                        dbContext.Batch.Add((Batch)batch);
                         dbContext.BatchDataEntry.Add(bde);
 
                         foreach (var tran in trans)
@@ -177,130 +178,18 @@ namespace ReadCsv
                             dbContext.ChecksDataEntry.Add(cde);
                         }
 
+                        dbContext.SaveChanges();
                         //lblTrans.Text = dbContext.Database.Log.ToString();
                     }
-                    dbContext.SaveChanges();
+                    
                 }
-            }
-            catch
-            {
-                throw;
-            }
+            //}
+            //catch
+            //{
+            //    throw;
+            //}
         }
 
-        private Batch CreateBatch(IEnumerable<BankTran> bankTrans)
-        {
-            int globalBatchID;
-
-            using (var dbContext = new ItemDBContext())
-            {
-                // Key is equal to the current max value plus one
-                globalBatchID = dbContext.Batch.Max(b => b.GlobalBatchID) + 1;
-            }
-
-            double amount;
-            double totalAmt = 0;
-            foreach (var tran in bankTrans)
-            {
-                if (double.TryParse(tran.PaymentAmount, out amount))
-                {
-                    totalAmt += amount;
-                }
-                else throw new Exception("Unable to parse transaction amount: " + tran.PaymentAmount);
-            }
-
-            DateTime curDate = DateTime.Now;
-            Batch batch = new Batch()
-            {
-                GlobalBatchID = globalBatchID,
-                BankID = 7,
-                LockboxID = bankTrans.Any(x => x.RecoveryPaymentType == "OVP")? 316 : 306, // overpaid batch or BRI batch
-                DESetupID = null,
-                ProcessingDate = curDate.Date,
-                BatchID = 203,
-                DepositStatus = 850,
-                BatchCueID = 0,// ???,
-                CutOffStatus = 0,
-                BatchMode = null,
-                SystemType = 0,
-                Priority = 1,
-                PrintLabels = 1,
-                BatchTypeCode = 50, // ???,
-                BatchTypeSubCode = 0,
-                BatchTypeText = null,
-                ConsolidationStatus = 0,
-                CARStatus = null,
-                KFIStatus = null,
-                FAXStatus = null,
-                FloatAssignmentStatus = 0,
-                ScanStatus = 21,
-                DEStatus = 0,
-                ExtractSequenceNumber = 0,
-                ImageExtractSequenceNumber = null,
-                DepositDate = curDate.Date,
-                CheckCount = bankTrans.Count(),
-                CheckAmount = (decimal)totalAmt,
-                StubCount = bankTrans.Count(),
-                StubAmount = (decimal)totalAmt,
-                CreationDate = curDate,
-                BillingTC = 0,
-                BillingRT = 0,
-                BillingAccount = 0,
-                BillingSerial = 0,
-                WorkgroupID = 1,
-                CurrencyCode = null,
-                MICRVerifyStatus = 4,
-                DEPrinted = null,
-                Is2Pass = 1,
-                UseCar = 1,
-                NoChecksEnclosed = 0,
-                Rejects = 0,
-                DepositTicketPrinted = false,
-                DateLastUpdated = curDate,
-                TransportID = null,
-                TrayID = null,
-                SubTrayID = null,
-                UseCustomer = 0,
-                DepositDDA = null,
-                EncodeComplete = null,
-                SplitsEnded = false,
-                SiteCode = null,
-                BitonalImageRemovalDate = null, // ist seems this removal dates are a 1 1/2 month later,
-                DataRemovalDate = null,
-                ModificationDate = curDate,
-                TransportBatchID = null,
-                GrayScaleImageRemovalDate = null,
-                ColorImageRemovalDate = null,
-                CWDBDataRemovalDate = null,
-                CWDBBitonalImageRemovalDate = null,
-                CWDBGrayScaleImageRemovalDate = null,
-                CWDBColorImageRemovalDate = null,
-                BatchName = null,
-                BatchTypeID = Guid.NewGuid(),
-                DecisionStatus = 0,
-                AllowDecisioning = false,
-                RequireDEBeforeTwoPass = false,
-                OriginalGlobalBatchID = null,
-                BatchExtractID = null,
-                IsImageExchange = false,
-                CaptureSiteCodeID = 10,
-                BatchSourceKey = 1,
-                NumOLDDaysRolled = 0,
-            };
-            return batch;
-        }
-        private BatchDataEntry CreateBatchDataEntry(int globalBatchID)
-        {
-            BatchDataEntry bde = new BatchDataEntry()
-            {
-                GlobalBatchID = globalBatchID,
-                ChecksStorageBoxNumber = null,
-                DocumentsStorageBoxNumber = null,
-                IronMountainBoxNumber = null
-            };
-
-            return bde;
-        }
 
         private IEnumerable<BankTran> FilterTransactions(PaymentHandler filter, IEnumerable<BankTran> trans)
         {
@@ -323,8 +212,6 @@ namespace ReadCsv
                     AmountDue = tran.AmountDue,
                     PaymentAmount = tran.PaymentAmount,
                     DueDate = tran.DueDate,
-                    FirstName = tran.FirstName,
-                    LastName = tran.LastName
                 };
 
 
@@ -337,8 +224,27 @@ namespace ReadCsv
                         bankTran.RecoveryPaymentType = par.Element("Value").Value;
                 }
 
+                foreach (var cont in tran.Contact)
+                {
+                    bankTran.FirstName = cont.Element("FirstName").Value;
+                    bankTran.LastName = cont.Element("LastName").Value;
+                }
+
                 yield return bankTran;
             }
+        }
+
+        private BatchDataEntry CreateBatchDataEntry(int globalBatchID)
+        {
+            BatchDataEntry bde = new BatchDataEntry()
+            {
+                GlobalBatchID = globalBatchID,
+                ChecksStorageBoxNumber = null,
+                DocumentsStorageBoxNumber = null,
+                IronMountainBoxNumber = null
+            };
+
+            return bde;
         }
 
         private IEnumerable<Transactions> CreateTransactions(IEnumerable<Checks> checks, Batch batch)
